23c8f6953e7f92a8cbf8397c8dc420e96f5053e9
Ahmed <zitouniahmed595@gmail.com>
2022-11-19 14:38:05 +0100

    lint check

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -2,8 +2,8 @@ import { fromJS } from 'immutable'
 
 
 const getImmutableObject = function(obj) {
-    const map = fromJS(obj);
-    return map
+  const map = fromJS(obj);
+  return map
 }
 
 export default getImmutableObject;
