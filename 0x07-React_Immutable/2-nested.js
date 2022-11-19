09797d3780675801c837111a50aa282bd287d851
Ahmed <zitouniahmed595@gmail.com>
2022-11-19 19:59:35 +0100

    access nested values

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,8 +1,6 @@
-import { Map } from 'immutable'
+import { fromJS, getIn } from 'immutable'
 
 export default function accessImmutableObject(object, array) {
-    var obj = Map(object);
-    var name = array[0];
-    var first = array[1]
-    return obj.get(name.first)
+    var cana = fromJS(object);
+    return cana.getIn(array, undefined)
 }
