215843261484c826eb11e3ad14a07d72d181ce43
Ahmed <zitouniahmed595@gmail.com>
2022-11-21 23:40:07 +0100

    lazy seq

---
+++ 0x07-React_Immutable/8-seq.js
@@ -0,0 +1,5 @@
+import { Seq } from 'immutable';
+
+export default function printBestStudents(grades) {
+  return console.log(Seq(grades).filter(x => x.score > 70).map(x => { return { ...x, firstName: x.firstName.charAt(0).toUpperCase() + x.firstName.substring(1), lastName: x.lastName.charAt(0).toUpperCase() + x.lastName.substring(1) }; }).toObject());
+}
